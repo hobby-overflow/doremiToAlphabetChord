699e588afad41572c32d9194ed44b13c94c87958
hobby-overflow <radio_old_boy_cq@yahoo.co.jp>
2022-04-25 13:34:01 +0900

    ドレミの入力をコマンドライン引数から入力するように変更した

--- doremiToAlphabetChord.nim
+++ doremiToAlphabetChord.nim
@@ -1,14 +1,12 @@
+import os
 import strutils
 
 const alphabetChord = ["A","B","C","D","E","F","G"]
-
-var kana1: string = "ドレミファソラシド"
+const kanaChord = ["ラ","シ","ド","レ","ミ","ファ","ソ"]
 
 proc doremiToAlphabetChord(kana: string): string =
     var alphabet = kana
     var cnt = 0
-    const alphabetChord = ["A","B","C","D","E","F","G"]
-    const kanaChord = ["ラ","シ","ド","レ","ミ","ファ","ソ"]
     for i in kanaChord:
         alphabet = replace(alphabet, i, alphabetChord[cnt])
         cnt += 1
@@ -16,6 +14,6 @@ proc doremiToAlphabetChord(kana: string): string =
     alphabet = replace(alphabet, "ー", " ")
     return alphabet
 
+# var kana1: string = "ドレミファソラシド"
+var kana1: string = os.commandLineParams()[0]
 echo kana1.doremiToAlphabetChord()
-echo kana2.doremiToAlphabetChord()
-
